6744e8623647465b205ba1ec73d8cf20f58fa859
vj2607 <68617033+vj2607@users.noreply.github.com>
2025-07-28 12:12:26 +0530

    Time: 21 ms (77.16%), Space: 45.7 MB (38.45%) - LeetHub

---
+++ 1312-minimum-insertion-steps-to-make-a-string-palindrome/1312-minimum-insertion-steps-to-make-a-string-palindrome.java
@@ -0,0 +1,31 @@
+class Solution {
+    public int minInsertions(String s) {
+       int n=s.length();
+       int[][] dp = new int[n][n];
+       
+       int max= 0;
+       for(int g=0;g<n;g++){
+          for(int i=0,j=g;j<n;i++,j++){
+             if(i==j)
+             dp[i][j]=1;
+             else if(i+1==j){
+                dp[i][j] = s.charAt(i)==s.charAt(j)?2:1;
+             }
+             else{
+                if(s.charAt(i)==s.charAt(j))
+                 dp[i][j]=2+dp[i+1][j-1];
+                else
+                dp[i][j]=Math.max(dp[i+1][j],dp[i][j-1]);
+             }
+
+             if(dp[i][j]!=0)
+             max=  Math.max(max,dp[i][j]);
+          }
+       }
+
+return n-max;
+
+    }
+
+   
+}
