407a5e25e65753b54a52e16187abffbe4a5dd235
vj2607 <68617033+vj2607@users.noreply.github.com>
2025-07-30 20:24:13 +0530

    Time: 6 ms (83.12%), Space: 44.7 MB (25.67%) - LeetHub

--- 0787-cheapest-flights-within-k-stops/0787-cheapest-flights-within-k-stops.java
+++ 0787-cheapest-flights-within-k-stops/0787-cheapest-flights-within-k-stops.java
@@ -9,41 +9,37 @@ class Solution {
     }
     public int findCheapestPrice(int n, int[][] flights, int src, int dst, int k) {
 
-       
-       
-        ArrayList<ArrayList<Pair>> adj = new ArrayList<>();
-         for(int i=0;i<n;i++){
-            adj.add(new ArrayList<>());
-         }
-         for(int i=0;i<flights.length;i++){
-            Pair p = new Pair(flights[i][1],flights[i][2]);
-            adj.get(flights[i][0]).add(p);
-         }
+       ArrayList<ArrayList<Pair>> adj = new ArrayList<>();
 
-         Queue<int[]> pq = new LinkedList<>();
-         int[] price = new int[n];
-         Arrays.fill(price,(int)1e9);
-         price[src]=0;
-         pq.add(new int[]{0,0,src});
+       for(int i=0;i<n;i++){
+        adj.add(new ArrayList<>());
+       }
+       for(int i=0;i<flights.length;i++){
+          adj.get(flights[i][0]).add(new Pair(flights[i][1],flights[i][2]));
+       }
 
-         while(!pq.isEmpty()){
-            int[] arr = pq.poll();
-            int stop = arr[1], cost = arr[0],node=arr[2];
-             
-          
-            
-          if(stop<=k){
-            for(Pair pair :adj.get(node)){      
-                if(price[pair.node]>cost+pair.price)    {
-                    price[pair.node]=cost+pair.price;
-         pq.add(new int[]{cost+pair.price,stop+1,pair.node});
-                  
+       Queue<int[]> q = new LinkedList<>();
+       q.add(new int[]{src,0,0});
+       int[] vis = new int[n];
+       Arrays.fill(vis,(int)1e9);
+       vis[src]=0;
+       while(!q.isEmpty()){
+        int[] arr  = q.poll();
+        int node = arr[0] ,stops = arr[2],cost=arr[1];
+         if(stops>k)
+          continue;
+
+          for(Pair p: adj.get(node)){
+            int adjNode = p.node;
+            int price =p.price;
+            if(vis[adjNode]>price+cost){
+                vis[adjNode]=price+cost;
+                q.add(new int[]{adjNode,price+cost,stops+1});
             }
           }
-         
-          }
-         
-    }
-    return price[dst]==(int)1e9?-1:price[dst];
-    }
+       }
+
+       return vis[dst]==(int)1e9?-1:vis[dst];
+
+}
 }
