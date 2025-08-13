715bd78104b8889bd25f5063ad3b9ce9b23edc8a
vj2607 <68617033+vj2607@users.noreply.github.com>
2025-08-13 13:21:12 +0530

    Time: 14 ms (86.6%), Space: 48.7 MB (22.9%) - LeetHub

--- 0044-wildcard-matching/0044-wildcard-matching.java
+++ 0044-wildcard-matching/0044-wildcard-matching.java
@@ -1,35 +1,35 @@
 class Solution {
     public boolean isMatch(String s, String p) {
-      int[][] dp = new int[s.length()][p.length()];
+      int[][] dp = new int[s.length()+1][p.length()+1];
       
-      return rec(s.length()-1,p.length()-1,s,p,dp);
+      return rec(s.length(),p.length(),s,p,dp);
     }
 
     boolean rec(int i ,int j,String s , String p ,int[][] dp ){
 
-        if(i<0 && j<0){
+        if(i==0 && j==0){
             return true; 
         }
-        if(i<0){
-            for(int k=j;k>=0;k--){
+        if(i==0){
+            for(int k=j-1;k>=0;k--){
                 if(p.charAt(k)!='*')
                  return false;
             }
             return true;
         }
-        if(j<0)
+        if(j==0)
         return false;
 
         if(dp[i][j]!=0)
          return dp[i][j]==1?true:false;
 
-        if(s.charAt(i)==p.charAt(j) || p.charAt(j)=='?'){
+        if(s.charAt(i-1)==p.charAt(j-1) || p.charAt(j-1)=='?'){
             boolean res =  rec(i-1,j-1,s,p,dp);
             dp[i][j]= res==true?1:-1;
           return res;
         }
      
-        if(p.charAt(j)=='*'){
+        if(p.charAt(j-1)=='*'){
             boolean res= rec(i,j-1,s,p,dp) || rec(i-1,j,s,p,dp);
               dp[i][j]= res==true?1:-1;
           return res;
