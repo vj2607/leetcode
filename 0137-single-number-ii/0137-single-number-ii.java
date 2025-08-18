b1cf4e56f9eafa8d7a48ff770e1cf1f172a8bdd5
vj2607 <68617033+vj2607@users.noreply.github.com>
2025-08-18 13:02:52 +0530

    Time: 0 ms (100%), Space: 45 MB (98.43%) - LeetHub

--- 0137-single-number-ii/0137-single-number-ii.java
+++ 0137-single-number-ii/0137-single-number-ii.java
@@ -1,17 +1,12 @@
 class Solution {
     public int singleNumber(int[] nums) {
-        int ans =0;
-        for(int bit=0;bit<32;bit++){
-             int count=0;
-            for(int i=0;i<nums.length;i++){
-                 if((nums[i] &(1<<bit)) !=0)
-                 count++;
-            }
-            if(count%3==1){
-                ans = ans| (1<<bit);
-            }
+        int ones =0 ,twos=0;
+
+        for(int i=0;i<nums.length;i++){
+          ones = (ones^nums[i]) & ~twos;
+          twos =(twos^nums[i]) & ~ones;
         }
 
-        return ans;
+        return ones;
     }
 }
