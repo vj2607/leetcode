a021ac4c1852bea2c69bdecbbaf7e22dec520deb
vj2607 <68617033+vj2607@users.noreply.github.com>
2025-08-18 14:26:33 +0530

    Time: 1 ms (99.74%), Space: 45.7 MB (81.71%) - LeetHub

---
+++ 0260-single-number-iii/0260-single-number-iii.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int[] singleNumber(int[] nums) {
+
+      long xor=0;
+      for(int i=0;i<nums.length;i++)
+        xor=xor^nums[i];
+
+        long rightmost= (xor & xor-1)^xor;
+        int b1=0,b2=0;
+        for(int i=0;i<nums.length;i++){
+          if((nums[i] & rightmost) !=0)
+          b1=b1^nums[i];
+          else
+          b2=b2^nums[i];
+        }
+
+        return new int[]{b1,b2};
+      
+        
+    }
+}
