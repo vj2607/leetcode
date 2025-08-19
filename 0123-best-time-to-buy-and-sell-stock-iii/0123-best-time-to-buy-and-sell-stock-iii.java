4cb1b012f3fe597baae17d4815d39fee56a6d970
vj2607 <68617033+vj2607@users.noreply.github.com>
2025-08-19 18:08:00 +0530

    Time: 221 ms (5%), Space: 99.4 MB (5%) - LeetHub

---
+++ 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.java
@@ -0,0 +1,28 @@
+class Solution {
+    public int maxProfit(int[] prices) {
+      Integer map[][][] = new Integer[prices.length][2][3];
+
+        return rec(0,1,prices,0,map);
+    }
+
+    int rec(int i,int buy,int[] nums,int trans,Integer map[][][]  ){
+            if(i==nums.length){
+                return 0;
+            }
+        
+
+        if(map[i][buy][trans]!=null)
+            return map[i][buy][trans];
+        int profit=0;
+        if(trans<2){
+        if(buy==1){
+            profit=Math.max(-nums[i]+rec(i+1,0,nums,trans,map),rec(i+1,1,nums,trans,map));
+        }
+        else{
+          profit=Math.max(nums[i]+rec(i+1,1,nums,trans+1,map),rec(i+1,0,nums,trans,map));  
+        }
+        }
+      
+        return map[i][buy][trans]= profit;
+    }
+}
