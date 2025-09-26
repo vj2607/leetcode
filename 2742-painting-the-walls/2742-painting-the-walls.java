0d058491d0a071d88cf04ca5dab16b07ff2e55af
vj2607 <68617033+vj2607@users.noreply.github.com>
2025-09-26 11:59:50 +0530

    Time: 11 ms (72.06%), Space: 47.7 MB (30.48%) - LeetHub

--- 2742-painting-the-walls/2742-painting-the-walls.java
+++ 2742-painting-the-walls/2742-painting-the-walls.java
@@ -1,9 +1,25 @@
 class Solution {
     public int paintWalls(int[] cost, int[] time) {
        int n = time.length;
-       Long[][] dp = new Long[n][n+1];
+       long[][] dp = new long[n+1][n+1];
 
-         return (int)rec(0,n,cost,time,n,dp);
+       for(int i=0;i<=n;i++){
+          dp[i][0]=0L;
+       }
+       for(int i=1;i<=n;i++){
+          dp[n][i]=(long)Integer.MAX_VALUE;
+       }
+
+       for(int i=n-1;i>=0;i--){
+        for(int j=0;j<=n;j++){
+
+            int newj =  Math.max(0, j - time[i] - 1);
+           
+            dp[i][j] = Math.min(cost[i] + dp[i+1][newj],dp[i+1][j]);
+        }
+       }
+
+         return (int)dp[0][n];
     }
 
      long rec(int i,int n , int[] cost, int[] time,int wallsToCover, Long[][] dp){
