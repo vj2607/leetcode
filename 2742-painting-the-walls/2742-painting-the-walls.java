d0d31d6bd02980874ffaeddb67ef1fe24edd5238
vj2607 <68617033+vj2607@users.noreply.github.com>
2025-09-26 12:00:06 +0530

    Time: 10 ms (75.56%), Space: 44.9 MB (92.38%) - LeetHub

--- 2742-painting-the-walls/2742-painting-the-walls.java
+++ 2742-painting-the-walls/2742-painting-the-walls.java
@@ -1,45 +1,27 @@
 class Solution {
     public int paintWalls(int[] cost, int[] time) {
        int n = time.length;
-       long[][] dp = new long[n+1][n+1];
+       long[] dp = new long[n+1];
 
-       for(int i=0;i<=n;i++){
-          dp[i][0]=0L;
-       }
+     
        for(int i=1;i<=n;i++){
-          dp[n][i]=(long)Integer.MAX_VALUE;
+          dp[i]=(long)Integer.MAX_VALUE;
        }
+       dp[0]=0;
 
        for(int i=n-1;i>=0;i--){
-        for(int j=0;j<=n;j++){
+        long temp[] = new long[n+1];
+        for(int j=1;j<=n;j++){
 
             int newj =  Math.max(0, j - time[i] - 1);
            
-            dp[i][j] = Math.min(cost[i] + dp[i+1][newj],dp[i+1][j]);
+            temp[j] = Math.min(cost[i] + dp[newj],dp[j]);
         }
+        dp=temp;
        }
 
-         return (int)dp[0][n];
+         return (int)dp[n];
     }
 
-     long rec(int i,int n , int[] cost, int[] time,int wallsToCover, Long[][] dp){
-
-        if(wallsToCover<=0){
-            return 0;
-        }
-            if(i>=n){
-                return Integer.MAX_VALUE;
-            }
-
-            if(dp[i][wallsToCover]!=null){
-                return dp[i][wallsToCover];
-            }
-
-       long pick = cost[i] + rec(i+1,n,cost,time,wallsToCover-time[i]-1,dp);
-
-       long notPick = rec(i+1,n,cost,time,wallsToCover,dp);
-
-        return dp[i][wallsToCover] =Math.min(pick,notPick);
-
-     }
+    
 }
